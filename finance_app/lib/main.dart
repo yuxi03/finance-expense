2f06a0a145c6ad40622ef5669734f057815be9bf
yuxi03 <n12165263@qut.edu.au>
2025-11-11 00:22:10 +1000

    file structure organize

--- finance_app/lib/main.dart
+++ finance_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:finance_app/pages/home_page.dart';
+import 'package:finance_app/features/transactions/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
